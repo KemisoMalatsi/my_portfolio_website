1b9c402deee1e2338272dd149ed8c2f580e8575f
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-07-03 11:25:52 +0200

    Updated index.html and index.js files: removed text from span element, added script tags, and implemented typing text effect with Typed.js library.

--- PORTFOLIO WEBSITE/index.js
+++ PORTFOLIO WEBSITE/index.js
@@ -3,4 +3,20 @@ let navbar = document.querySelector('.navbar');
 
 menu.Onclick = () => {
     menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+// Typing Text
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Software Engineer', 'Fullstack Developer', 'Freelancer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true, 
+  });
